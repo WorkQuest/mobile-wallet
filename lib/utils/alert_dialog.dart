9db8844ad273c2880ae94e68d107599bbaff973f
Alexander <48985323+Dogayo@users.noreply.github.com>
2022-07-05 15:16:49 +0700

    Add show alert tx confirm

--- lib/utils/alert_dialog.dart
+++ lib/utils/alert_dialog.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:easy_localization/src/public_ext.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:workquest_wallet_app/constants.dart';
 import 'package:workquest_wallet_app/widgets/alert_success.dart';
 
 class AlertDialogUtils {
@@ -59,8 +60,7 @@ class AlertDialogUtils {
     }
   }
 
-  static Future<void> showLoadingDialog(BuildContext context,
-      {String? message}) async {
+  static Future<void> showLoadingDialog(BuildContext context, {String? message}) async {
     final content = Column(
       mainAxisSize: MainAxisSize.min,
       children: [
@@ -155,48 +155,16 @@ class AlertDialogUtils {
     required Color? colorOk,
   }) {
     showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (context) {
-          if (Platform.isIOS) {
-            return CupertinoAlertDialog(
-              title: title,
-              content: content,
-              actions: [
-                if (needCancel)
-                  CupertinoButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                      if (onTabCancel != null) {
-                        onTabCancel.call();
-                      }
-                    },
-                    child: Text(
-                      titleCancel ?? 'meta'.tr(gender: 'cancel'),
-                      style: TextStyle(color: colorCancel ?? Colors.black),
-                    ),
-                  ),
-                CupertinoButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                    if (onTabOk != null) {
-                      onTabOk.call();
-                    }
-                  },
-                  child: Text(
-                    titleOk ?? 'Ok',
-                    style: TextStyle(color: colorOk ?? Colors.black),
-                  ),
-                ),
-              ],
-            );
-          }
-          return AlertDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        if (Platform.isIOS) {
+          return CupertinoAlertDialog(
             title: title,
             content: content,
             actions: [
               if (needCancel)
-                TextButton(
+                CupertinoButton(
                   onPressed: () {
                     Navigator.pop(context);
                     if (onTabCancel != null) {
@@ -205,10 +173,10 @@ class AlertDialogUtils {
                   },
                   child: Text(
                     titleCancel ?? 'meta'.tr(gender: 'cancel'),
-                    style: TextStyle(color: colorOk ?? Colors.black),
+                    style: TextStyle(color: colorCancel ?? Colors.black),
                   ),
                 ),
-              TextButton(
+              CupertinoButton(
                 onPressed: () {
                   Navigator.pop(context);
                   if (onTabOk != null) {
@@ -217,11 +185,134 @@ class AlertDialogUtils {
                 },
                 child: Text(
                   titleOk ?? 'Ok',
-                  style: TextStyle(color: colorCancel ?? Colors.black),
+                  style: TextStyle(color: colorOk ?? Colors.black),
                 ),
               ),
             ],
           );
-        });
+        }
+        return AlertDialog(
+          title: title,
+          content: content,
+          actions: [
+            if (needCancel)
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  if (onTabCancel != null) {
+                    onTabCancel.call();
+                  }
+                },
+                child: Text(
+                  titleCancel ?? 'meta'.tr(gender: 'cancel'),
+                  style: TextStyle(color: colorOk ?? Colors.black),
+                ),
+              ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                if (onTabOk != null) {
+                  onTabOk.call();
+                }
+              },
+              child: Text(
+                titleOk ?? 'Ok',
+                style: TextStyle(color: colorCancel ?? Colors.black),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  static void showAlertTxConfirm(
+    BuildContext context, {
+    required String typeTx,
+    required String addressTo,
+    required String amount,
+    required String fee,
+    required String tokenSymbol,
+    required String tokenSymbolFee,
+    required Function() onTabOk,
+  }) {
+    AlertDialogUtils.showAlertDialog(
+      context,
+      title: Text(typeTx),
+      content: Container(
+        width: 300,
+        padding: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: Colors.blue.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(6.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              "Recipient's address",
+              style: TextStyle(
+                fontSize: 17.0,
+              ),
+            ),
+            Text(
+              addressTo,
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                fontSize: 15.0,
+                color: Color(0xFF7C838D),
+              ),
+            ),
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    "wallet.amount".tr(),
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                      fontSize: 17.0,
+                    ),
+                  ),
+                  Text(
+                    "$amount $tokenSymbol",
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                      fontSize: 15.0,
+                      color: Color(0xFF7C838D),
+                    ),
+                  ),
+                ],
+              ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            const Text(
+              "Transaction fee",
+              style: TextStyle(
+                fontSize: 17.0,
+              ),
+            ),
+            Text(
+              "$fee $tokenSymbolFee",
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                fontSize: 15.0,
+                color: Color(0xFF7C838D),
+              ),
+            ),
+          ],
+        ),
+      ),
+      needCancel: true,
+      titleCancel: null,
+      titleOk: 'meta.confirm'.tr(),
+      onTabCancel: null,
+      onTabOk: () => onTabOk(),
+      colorCancel: AppColor.enabledButton,
+      colorOk: Colors.red,
+    );
   }
 }
