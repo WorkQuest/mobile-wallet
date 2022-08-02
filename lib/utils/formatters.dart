f3dfa14070ca8c6a0f14572de951a0d4e632cd13
Alexander <48985323+Dogayo@users.noreply.github.com>
2022-08-02 15:36:43 +0700

    Add formatters

---
+++ lib/utils/formatters.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/services.dart';
+
+class DecimalFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    String truncated = newValue.text;
+    TextSelection newSelection = newValue.selection;
+    if (newValue.text.contains(",")) {
+      truncated = newValue.text.replaceFirst(RegExp(','), '.');
+    }
+    return TextEditingValue(
+      text: truncated,
+      selection: newSelection,
+    );
+  }
+}
